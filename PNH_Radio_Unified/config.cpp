072db63979c8bb1341fcceb0f841a9644808af01
PinheiroW <152659864+PinheiroW@users.noreply.github.com>
2026-02-12 18:32:59 -0300

    att
    Atualização"config.cpp / dayzgame.c"

--- PNH_Radio_Unified/config.cpp
+++ PNH_Radio_Unified/config.cpp
@@ -1,47 +1,50 @@
 class CfgPatches
 {
-	class PNH_Radio_Unified
+	class PNH_Radio
 	{
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 0.1;
-		requiredAddons[] = {
-			"DZ_Data",
-			"DZ_Scripts",
-			"DZ_Sounds_Effects",
-			"DZ_Gear_Books"
-		};
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={"DZ_Data","DZ_Scripts","DZ_Sounds_Effects"};
 	};
 };
 
 class CfgMods
 {
-	class PNH_Radio_Unified
+	class PNH_Radio
 	{
-		type = "mod";
-		dependencies[] = {"Game", "World", "Mission"};
+		dir="PNH_Radio";
+		name="PNH Radio System";
+		credits="Pinheiro";
+		author="Pinheiro";
+		version="3.1";
+		type="mod";
+		dependencies[]={"GameLib","Game","World","Mission"};
 		
 		class defs
 		{
-			class engineScriptModule
+			class gameLibScriptModule
 			{
-				value = "";
-				files[] = {"PNH_Radio_Unified/scripts/1_Core"};
+				value="";
+				files[]={"PNH_Radio/scripts/2_GameLib"};
 			};
-			class gameScriptModule
-			{
-				value = "";
-				files[] = {"PNH_Radio_Unified/scripts/3_Game"};
+			class gameScriptModule 
+			{ 
+				value=""; 
+				files[]={
+					"PNH_Radio/scripts/1_Core",
+					"PNH_Radio/scripts/3_Game"
+				}; 
 			};
-			class worldScriptModule
-			{
-				value = "";
-				files[] = {"PNH_Radio_Unified/scripts/4_World"};
+			class worldScriptModule 
+			{ 
+				value=""; 
+				files[]={"PNH_Radio/scripts/4_World"}; 
 			};
-			class missionScriptModule
-			{
-				value = "";
-				files[] = {"PNH_Radio_Unified/scripts/5_Mission"};
+			class missionScriptModule 
+			{ 
+				value=""; 
+				files[]={"PNH_Radio/scripts/5_Mission"}; 
 			};
 		};
 	};
--- PNH_Radio_Unified/scripts/3_Game/BitWise/dayzgame.c
+++ PNH_Radio_Unified/scripts/3_Game/BitWise/dayzgame.c
@@ -1,70 +1,69 @@
 /*
- * BitWise Framework
- * https://github.com/antihax/BitWiseFramework
- * © 2023 antihax
- *
- * This work is licensed under the Creative Commons Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0)
- * To view a copy of this license, visit https://creativecommons.org/licenses/by-nd/4.0/
- *
+ * PNH_Radio - Unified DayZGame Module
+ * Unifica BitWise Framework e RadioNotifications
  */
+
 #ifdef BITWISEDEBUG
 void BITWISE_SERVERTEST1(PlayerIdentity sender, ParamsReadContext ctx) {
 	Print("BITWISE_SERVERTEST1");
-	test_Endpoints();
-}
-void BITWISE_SERVERTEST2(PlayerIdentity sender, ParamsReadContext ctx) {
-	Print("BITWISE_SERVERTEST2");
-	test_Endpoints();
-}
-void BITWISE_SERVERTEST3(PlayerIdentity sender, ParamsReadContext ctx) {
-	Print("BITWISE_SERVERTEST3");
-	test_Endpoints();
-}
-void BITWISE_SERVERTEST4(PlayerIdentity sender, ParamsReadContext ctx) {
-	Print("BITWISE_SERVERTEST4");
-	test_Endpoints();
-}
-
-void BITWISE_CLIENTTEST1(PlayerIdentity sender, ParamsReadContext ctx) {
-	Print("BITWISE_CLIENTTEST1");
 }
 #endif
 
 modded class DayZGame {
+	
 	void DayZGame() {
-#ifdef BITWISEDEBUG
-#ifdef SERVER
+		// --- Inicialização do Rádio ---
+		RadioNotificationSettings.Init();
+		
+		#ifdef SERVER
+			RadioNotificationSettings.GetSettings().Load();
+			RadioNotificationSettings.GetSettings().InitOneTimePad();
+			
+			// Registra os canais de comunicação no BitWise
+			GetBitWiseManager().RegisterEndpoint("RadioNotifications", "CONFIGURATION");
+			GetBitWiseManager().RegisterEndpoint("RadioNotifications", "RADIONOTIFICATION");
+			GetBitWiseManager().RegisterEndpoint("RadioNotifications", "RADIONOTIFICATIONALARM");
+			
+			// Cria o Manager Global do Servidor
+			g_RadioNotificationManager = new RadioNotificationManager();
+		#else
+			// Lógica de Cliente
+			KeysToUIElements.Init(); 
+			RadioNotificationSettings.GetSettings().DefaultSettings();
+			g_RadioNotificationClientHandler = new RadioNotificationClientHandler();
+		#endif
 
-		GetBitWiseManager().RegisterEndpoint("BITWISE", "servertest1");
-		GetBitWiseManager().RegisterEndpoint("BITWISE", "servertest2");
-		GetBitWiseManager().RegisterEndpoint("BITWISE", "servertest3");
-		GetBitWiseManager().RegisterEndpoint("BITWISE", "servertest4");
-		GetBitWiseManager().ConnectEndpoint("BITWISE", "clienttest1", ScriptCaller.Create(BITWISE_CLIENTTEST1));
-#else
-		GetBitWiseManager().ConnectEndpoint("BITWISE", "servertest1", ScriptCaller.Create(BITWISE_SERVERTEST1));
-		GetBitWiseManager().ConnectEndpoint("BITWISE", "servertest2", ScriptCaller.Create(BITWISE_SERVERTEST2));
-		GetBitWiseManager().ConnectEndpoint("BITWISE", "servertest3", ScriptCaller.Create(BITWISE_SERVERTEST3));
-		GetBitWiseManager().ConnectEndpoint("BITWISE", "servertest4", ScriptCaller.Create(BITWISE_SERVERTEST4));
-#endif
-#endif
+		// --- Inicialização de Debug do BitWise (Opcional) ---
+		#ifdef BITWISEDEBUG
+			#ifdef SERVER
+				GetBitWiseManager().RegisterEndpoint("BITWISE", "servertest1");
+			#endif
+		#endif
+	}
+
+	override void DeferredInit() {
+		// Detecta os sons configurados no config.cpp
+		RadioNotificationSettings.GetSettings().DetectSoundSets();
+		super.DeferredInit();
 	}
 
 	override void OnRPC(PlayerIdentity sender, Object target, int rpc_type, ParamsReadContext ctx) {
-#ifdef BITWISEDEBUG
-		int prev = TickCount(0);
-#endif
-
+		// Redireciona mensagens do BitWise para o Manager correto
 		if (rpc_type == RPC_ANTIHAX_BITWISE)
+		{
 			GetBitWiseManager()._OnRPC(sender, target, ctx);
+		}
 		else
+		{
 			super.OnRPC(sender, target, rpc_type, ctx);
-
-#ifdef BITWISEDEBUG
-		int time = TickCount(prev);
-		int estimatedMil = (int)(time / 10000.0);
-		Print("RPC: " + rpc_type + " took " + time + " ticks (" + estimatedMil + "ms) " + EnumTools.EnumToString(ERPCs, rpc_type));
-#endif
+		}
 	}
 
-	void ~DayZGame() {}
-}
+	void ~DayZGame() {
+		#ifdef SERVER
+			g_RadioNotificationManager = null;
+		#else
+			g_RadioNotificationClientHandler = null;
+		#endif
+	}
+};
